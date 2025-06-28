510457eab774b8cd1d8a50721551d00eda849259
vcodezindia <vcodezservices@gmail.com>
2025-06-28 15:50:16 +0530

    Update server.py

--- server.py
+++ server.py
@@ -301,4 +301,5 @@ def internal_error(error):
     return render_template('500.html'), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
